a5a123d0c76d0ebfc0b818c3e7b819462d67e1f1
SRAKIB17 <rakibulssc5@gmail.com>
2022-02-06 02:36:33 +0600

    aa ;

--- js/addCart.js
+++ js/addCart.js
@@ -6,6 +6,7 @@ let cart = [
         quantity: 'Quantity'
     }
 ]
+let buyProduct = []
 
 
 function auto() {
--- js/addCartBuyNow.js
+++ js/addCartBuyNow.js
@@ -1,5 +1,4 @@
 function addToCart(countColumn) {
-    let getCart = cart;
 
     // get cart count 
     let getCartValue = Number(document.getElementById('cartCount').value);
@@ -38,10 +37,44 @@ function addToCart(countColumn) {
     cart[cart.length-1].price = cartPrice;        cart[cart.length-1].quantity = cartQuantity;
     AutoHiddenCartDetailsUpdate()
 }
-function buyNow(getId) {
-        document.getElementById('BuyNow-'+getId.value).style.display = 'block';
-        document.getElementById('BuyNow-'+value.value).style.transform = 'scale(1.4)';
-        
+function buyNow(ProductId) {
+    // input getId
+    let getId = ProductId.value;
+    document.getElementById('BuyNow-'+getId).style.display = 'block';
+    // document.getElementById('BuyNow-'+value.value).style.transform = 'scale(1.4)';
+    
+    let countBuyProduct = Number(document.getElementById(getId).value);
+
+    // separate for get item code with item Main Product;
+        // get Image
+    let buyImage = document.getElementById('getProductImage-'+getId).value;
+        // get model or product 
+    let buyProductModel = document.getElementById('getProductModel-'+getId).value;
+        // get quantity
+    let buyQuantity = countBuyProduct;
+        // get item price
+    let buyPrice = document.getElementById('getProductPrice-'+getId).value;
+    
+    // count total 
+    let totalProductValue = buyQuantity * buyPrice;
+    // add item on cart Object
+    let creatBuyObject =  {
+        image:'none',
+        product: 'none',
+        price: 0,
+        quantity: 0
+    }
+    
+    buyProduct.push(creatBuyObject);
+    buyProduct[buyProduct.length-1].image = buyImage;
+    buyProduct[buyProduct.length-1].product = buyProductModel;
+    buyProduct[buyProduct.length-1].price = buyPrice;
+    buyProduct[buyProduct.length-1].quantity = buyQuantity;
+
+    // create table
+    let table = '<table class="w-100"><caption>Show Details</caption><tr><td><img src="images/'+buyImage+'" alt="" class="cartImage"></td><td colspan="2">'+buyProductModel+'</td></tr><tr><td>Quantity:  </td><td class="text-center">'+buyQuantity+'</td><td rowspan="3"><button class="btn bg-warning" value="'+0+'" onclick="addToCart(this)" style="font-size:24px;color: whitesmoke;">Buy</button></td></tr><tr><td>Price:</td><td>'+buyPrice+'</td></tr><tr><td>Total:</td><td>'+totalProductValue+'</td></tr></table>'
+
+    document.getElementById('buyNowBody-'+getId).innerHTML = table;
 }
 function closeBuy(getId) {
         document.getElementById('BuyNow-'+getId.value).style.display = 'none';
