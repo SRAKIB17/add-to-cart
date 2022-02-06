98ce25a3ad16ca0e9906e44089591b7e0b2f20f5
SRAKIB17 <rakibulssc5@gmail.com>
2022-02-07 01:52:59 +0600

    ss

--- js/search.js
+++ js/search.js
@@ -1,11 +1,13 @@
 // searchShow with display search blox
+
 function searchShow() {
     document.getElementById('showCart').style.display = 'none'
     document.getElementById('searchId').style.display = 'inline'
     document.getElementById('showSearchBox').style.display = 'none'
     document.getElementById('closeSearchBox').style.display = 'inline'
     document.getElementById('resultDiv').style.display = 'block'
-
+    // auto call 
+    searchControl('start')
 }
 // hidden all of search and show searchShow icon 
 function searchClose() {
@@ -13,15 +15,27 @@ function searchClose() {
     document.getElementById('searchId').style.display = 'none'
     document.getElementById('closeSearchBox').style.display = 'none'
     document.getElementById('showSearchBox').style.display = 'inline'
+    searchControl('stop')
 }
 // search product function
+function searchControl(signal) {
+    let autoTime = setInterval(searchProduct,1000);
+    if (signal == 'start') {
+        return autoTime
+    }
+    else {
+        let clear = clearInterval(autoTime);
+        return clear;
+    }
+}
 var searchQuiry = ''
 
-function searchProduct(search) {
-    let resultArray = result
+function searchProduct() {
     let resultShow = '<tr class="w-100"><th>Product</th><th>Price</th><th>Details</th><th>Category</th></tr>'
+    
     searchQuiry = document.getElementById('searchId').value;
-    getResult(searchQuiry);
+    let resultArray = getResult(searchQuiry);
+
     if (resultArray.length == 0) {
         resultShow = '<div><h2 class="searchNotFound">Not Found</h2></div>'
     }
@@ -33,6 +47,6 @@ function searchProduct(search) {
     }
 
     document.getElementById('resultDiv').innerHTML = resultShow;
-    setInterval(searchProduct,2000);
+    
     return result = [];
 }
